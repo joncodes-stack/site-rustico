7d3e4ad56687495789fa65d340e9b3ae0f5c7b37
jonatas.paz <jonatas.paz@imusica.com.br>
2024-12-04 10:57:56 -0300

    feat: finalizando projeto com o menu responsivo.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var btnMenu = document.querySelector('.btn-menu');
 
 function menuMobile (){
     menu.classList.toggle('show')
+    btnMenu.classList.toggle('x')
 }
 
 btnMenu.addEventListener('click', menuMobile);
